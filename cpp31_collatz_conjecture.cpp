8e5ade98dbf4d8f1bdb4cc3337707c3fdff8311b
Yong Hua Tang <tang.yonghua010111@gmail.com>
2021-01-20 17:33:43 +0800

    update

---
+++ cpp31_collatz_conjecture.cpp
@@ -0,0 +1,13 @@
+# include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Please enter a number: ";
+    int n;
+    cin >> n;
+    while (n != 1) {
+        // not completed
+    }
+
+    return;
+}

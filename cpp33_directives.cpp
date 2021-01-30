bff74ed0455dcad63358334c7e4c795169fbe494
Yong Hua Tang <tang.yonghua010111@gmail.com>
2021-01-30 11:20:48 +0800

    update

--- cpp33_directives.cpp
+++ cpp33_directives.cpp
@@ -1,10 +1,15 @@
 # include <bits/stdc++.h>
 using namespace std;
 
-// C++ preprocessor directives, or just simply macro
+// C++ preprocessor directives, or just simply 'macro'
+# define ll long long
+# define size(arr) sizeof(arr)/sizeof(arr[0])
 # define getmax(a, b) (a>b?a:b)  // ternary operator, (expression ? true : false) 
+
+ll num = 123456789123456789;
 
 int main() {
     cout << getmax(2, 3) << endl;
+    cout << num; 
     return 0;
 }

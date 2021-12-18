7a43bbfbe62e31e021be0337388458a655ba3c92
Yong Hua Tang <tang.yonghua010111@gmail.com>
2021-12-18 21:42:33 +0800

    update cpp40

---
+++ cpp40_template_decltype.cpp
@@ -0,0 +1,30 @@
+// template and decltype
+// 10 December 2021 13:35
+
+#include <bits/stdc++.h>
+using namespace std;
+
+template<typename T1, typename T2> T1 addition(T2 a, T2 b) {
+    return a + b;
+}
+
+void increment(int& val) {
+    val++;
+}
+
+int main() {
+    // cout << addition<float, int>(4, 5) << endl;
+    // cout << addition(4, 5) << endl;
+    // echo([](const int value){return value + 1;}(9));
+
+    int p = 10;
+    // int& y = p;
+    // echo(p);
+    // echo(y);
+    // echo(&p);
+    // echo(&y);
+    cout << p << endl;
+    increment(p);
+    cout << p << endl;
+    return 0;
+}

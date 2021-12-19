60124d67ef67be9697f64aee54f8800e6867f8bf
Yong Hua Tang <tang.yonghua010111@gmail.com>
2021-12-19 16:38:13 +0800

    cpp004

--- cpp004_string.cpp
+++ cpp004_string.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main() {
     string foo = "Hello world!";
-    char[] bar = "Hello world!"
+    char bar[] = "Hello world!";
     cout << foo << endl;
     cout << bar << endl;
     return 0;

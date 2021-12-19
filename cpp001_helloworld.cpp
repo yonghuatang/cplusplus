395681f5e4cbccb8250bc297b4895f9fec634d48
James Tang <70836155+yonghuatang@users.noreply.github.com>
2021-12-19 15:58:36 +0800

    Update and rename cpp1_helloworld.cpp to cpp001_helloworld.cpp

---
+++ cpp001_helloworld.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    cout << "Hello World! \n";  // This is a comment.
+    cout << "I am learning C++! \n";
+    cout << n;
+    return 0;
+}

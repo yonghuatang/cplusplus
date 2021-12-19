94126c255da0cfa06edce0f0861cc87795b1d105
James Tang <70836155+yonghuatang@users.noreply.github.com>
2021-12-19 16:33:18 +0800

    Update and rename cpp5.cpp to cpp005_string.cpp

---
+++ cpp005_string.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string foo = "Hello world!";
+    char[] bar = "Hello world!"
+    cout << foo << endl;
+    cout << bar << endl;
+    return 0;
+}
--- cpp5.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <string>
-using namespace std;
-
-int main() {
-    string x = "Hello world!";
-    cout << x;
-    return 0;
-}

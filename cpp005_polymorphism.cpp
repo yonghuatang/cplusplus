ed08860056726cd51130ab381f8309d2bfc68ee0
James Tang <70836155+yonghuatang@users.noreply.github.com>
2021-12-21 20:42:20 +0800

    Update and rename cpp6.cpp to cpp005_polymorphism.cpp

---
+++ cpp005_polymorphism.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string foo = "Hello world! ";
+    string bar = "Nice to meet you!";
+    int x = 4;
+    int y = 5;
+    cout << foo + bar << endl; // concatenate
+    cout << x + y << endl;  // integer addition
+    return 0;
+}

db45153c4fcd27afd7e5c5cd37ec74caa132f185
James Tang <70836155+yonghuatang@users.noreply.github.com>
2021-12-21 20:47:43 +0800

    Update and rename cpp8.cpp to cpp006_ifelse.cpp

---
+++ cpp006_ifelse.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x;
+    int y;
+    cout << "Please enter an integer 'x': ";
+    cin >> x;
+    cout << "Please enter an integer 'y': ";
+    cin >> y;
+    if (x >= y) {
+        cout << x << " is greater or equal than " << y;
+    }
+    else {
+        cout << x << " is smaller than " << y;
+    }
+    return 0;
+}
+
+
+// ternary if-else statement
+// int main() {
+//     int x;
+//     int y;
+//     cout << "Please enter an integer 'x': ";
+//     cin >> x;
+//     cout << "Please enter an integer 'y': ";
+//     cin >> y;
+//     cout << x << " is " << (x>=y) ? "greater or equal" : "smaller" << " than " << y;
+//     return 0;
+// }

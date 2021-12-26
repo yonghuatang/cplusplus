60d6b952586ab81444d1a3ba6f989859c8087ec8
James Tang <70836155+yonghuatang@users.noreply.github.com>
2021-12-26 11:55:29 +0800

    Update cpp36_set.cpp

--- cpp36_set.cpp
+++ cpp36_set.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
     set<int> mySet = {1, 2, 4, 4, 4, 5, 6, 8, 8, 9};
-    for (auto &element: mySet) {
+    for (auto& element: mySet) {
         cout << element << " ";
     }
     cout << endl;

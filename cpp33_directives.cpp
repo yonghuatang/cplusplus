11b842f90c5293f5fe95269e6630958d20563fc3
James Tang <70836155+yonghuatang@users.noreply.github.com>
2021-12-26 15:27:02 +0800

    Update cpp33_directives.cpp

--- cpp33_directives.cpp
+++ cpp33_directives.cpp
@@ -1,10 +1,23 @@
-# include <bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-// C++ preprocessor directives, or just simply 'macro'
-# define ll long long
-# define size(arr) sizeof(arr)/sizeof(arr[0])
-# define getmax(a, b) (a>b?a:b)  // ternary operator, (expression ? true : false) 
+// C++ preprocessor directives, or just simply 'macro'. Eaxmples:
+#define PI 3.142
+#define ll long long
+#define size(arr) sizeof(arr)/sizeof(arr[0])
+#define getmax(a, b) (a>b?a:b)  // ternary operator, (expression ? true : false) 
+#define echo(x) std::cout << x << std::endl
+
+/*
+Other directives:
+#ifdef
+#ifndef
+#undef
+#endif
+#if
+#elif
+#else
+*/
 
 ll num = 123456789123456789;
 

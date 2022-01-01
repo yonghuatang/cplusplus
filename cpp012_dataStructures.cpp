59a825acd4dae327e0d583060682632a4fc1b31f
Yong Hua Tang <tang.yonghua010111@gmail.com>
2022-01-02 02:08:32 +0800

    update files

--- cpp012_dataStructures.cpp
+++ cpp012_dataStructures.cpp
@@ -3,21 +3,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-/* Three ways to declare/initialise an array */
+/* === Three ways to declare/initialise an array === */
 
 int arr1[5];  // array of size 5 storing integers
 int arr2[] = {1, 2, 3, 4, 5};  // explicit initialisation
 int arr3[5] = {1, 2, 3, 4, 5};  // declare the size and initialise elements at the same time
 
 
-/* Vectors are dynamic arrays */
+/* === Vectors are dynamic arrays === */
 
 vector<int> vec;
 // vec.push_back()
 // vec.size()
 
 
-/* Stack: last-in-first-out */
+/* === Stack: last-in-first-out === */
 
 stack<int> myStack;
 // myStack.push()  push into the stack
@@ -26,7 +26,7 @@ stack<int> myStack;
 // myStack.empty()  checks if the stack is empty
 
 
-/* Queue: first-in-first-out, enqueue and dequeue */
+/* === Queue: first-in-first-out, enqueue and dequeue === */
 
 queue<int> q;
 // myStack.push()  push into the stack
@@ -35,7 +35,7 @@ queue<int> q;
 // myStack.empty()  checks if the stack is empty
 
 
-/* Priority queue: queue with sorting and priority dequeueing, default is max heap*/
+/* === Priority queue: queue with sorting and priority dequeueing, default is max heap === */
 
 priority_queue<int> pq1;
 // pq.push()  push into the stack
@@ -53,9 +53,35 @@ struct comp {
 priority_queue<int, vector<int>, comp> pq2;
 
 
-/*  */
+/* === Set: container that stores unique elements === */
+
+set<int> s;
+// s.insert()
+
+
+/* === Map: stores unique keys in sorted order === */
+
+map<char, int> myMap;
+
+// Initialise
+// myMap['a'] = 10;
+// myMap['b'] = 20;
+// myMap['c'] = 30;
+// myMap['d'] = 40;
+// map<char, int>::iterator it;
+// for (it=myMap.begin(); it!=myMap.end(); it++) {
+//     cout << it->first << "=>" << it->second << endl;
+// }
 
 int main() {
+    myMap['a'] = 10;
+    myMap['b'] = 20;
+    myMap['c'] = 30;
+    myMap['d'] = 40;
+    map<char, int>::iterator it;
+    for (it=myMap.begin(); it!=myMap.end(); it++) {
+        cout << it->first << " => " << it->second << endl;
+    }
     return 0;
 }
 

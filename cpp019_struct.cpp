4ad27924f69de37185144b51129fe916e057a4f7
James Tang <70836155+yonghuatang@users.noreply.github.com>
2022-01-06 20:08:27 +0800

    Update and rename cpp30_struct.cpp to cpp019_struct.cpp

--- cpp019_struct.cpp
+++ cpp019_struct.cpp
@@ -2,17 +2,20 @@
 using namespace std;
 
 // A struct (short for structure) allows us to group variables of mixed data types together into a single unit.
+// unlike class, struct members are public by default, and normally doesn't have methods (or functions).
 
 struct Employee {
     int age;
     string first_name;
     string last_name;
     float salary;
-}; // semicolon!!
+};    // semicolon!!
 
-Employee joe{21, "joe", "mozart", 30000};
 
 int main() {
+    Employee joe{21, "joe", "mozart", 30000};
+    cout << joe.age;
+    joe.age = 22;
     cout << joe.age;
     return 0;
 }

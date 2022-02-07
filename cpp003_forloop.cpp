c20bf190d2ebcda02d9f45f46b747d57aaddb17a
James Tang <70836155+yonghuatang@users.noreply.github.com>
2022-02-07 22:58:12 +0800

    Update cpp003_forloop.cpp

--- cpp003_forloop.cpp
+++ cpp003_forloop.cpp
@@ -1,8 +1,10 @@
-# include <iostream>
+// Just a simple for-loop
+
+#include <iostream>
 using namespace std;
 
 int main() {
-    for (int i = 0; i <= 10; ++i) {
+    for (int i=0; i<=10; i++) {
         cout << i << endl;
     }
     return 0;
